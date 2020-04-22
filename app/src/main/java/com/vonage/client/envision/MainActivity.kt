649c6506b91fc35e8b1207f2b35ef96394660f25
igor.wojda <igor.wojda@gmail.com>
2020-04-22 16:03:29 +0200

    Init commit

---
+++ app/src/main/java/com/vonage/client/envision/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.vonage.client.envision
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vonage.client.envision.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
